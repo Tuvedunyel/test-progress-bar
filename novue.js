f575689530fb7182ceaccc7542a5e6f047613348
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-08-25 08:59:32 +0200

    start

---
+++ novue.js
@@ -0,0 +1,25 @@
+{
+  /* <svg
+              v-bind:width="sqSize + 'px'"
+              v-bind:height="sqSize + 'px'"
+              v-bind:viewBox="viewBox"
+            >
+              <circle
+                class="circle-background"
+                v-bind:cx="sqSize / 2"
+                v-bind:cy="0"
+                v-bind:r="radius"
+                v-bind:transform="rotate"
+                v-bind:stroke-width="strokeWidthpx"
+              />
+              <circle
+                class="circle-progress"
+                v-bind:cx="sqSize / 2"
+                v-bind:cy="0"
+                v-bind:r="radius"
+                v-bind:stroke-width="strokeWidthpx"
+                v-bind:transform="rotate"
+                v-bind:style="strokeDashArray"
+              />
+            </svg> */
+}
