294a68e9abdce4b3a0d1aa046fa7a86b4a50bfa7
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-08-25 11:24:03 +0200

    class object part one working

--- novue.js
+++ novue.js
@@ -9,39 +9,37 @@ class ProgressBar {
       (this.percentage = percentage),
       (this.strokeWidth = strokeWidth);
   }
-  viewBod() {
+  viewBox() {
     return `0 0 ${this.sqSize} ${this.sqSize}`;
   }
   radius() {
     return (this.sqSize - this.strokeWidth) / 2;
   }
   dashArray() {
-    return this.radius * Math.PI * 2;
+    return this.radius() * Math.PI * 2;
   }
   dashOffset() {
-    return this.dashArray - (this.dashArray * (this.percentage / 2)) / 100;
+    return this.dashArray() - (this.dashArray() * (this.percentage / 2)) / 100;
   }
   rotate() {
-    return `rotage(-180 ${this.sqSize / 2} ${this.sqSize / 2})`;
+    return `rotate(-180 ${this.sqSize / 2} ${this.sqSize / 2})`;
   }
   strokeWidthpx() {
     return this.strokeWidth + "px";
   }
   strokeDashArray() {
-    return `stroke-dasharray: ${this.dashArray}; stroke-dashoffset: ${this.dashOffset}`;
+    return `stroke-dasharray: ${this.dashArray()}; stroke-dashoffset: ${this.dashOffset()}`;
   }
   svgGenerator() {
-    return `<svg width="${this.sqSize}px" height="${this.sqSize}px" viewBox="${
-      this.viewBox
-    }">
-        <circle class="circle-background" cx="${this.sqSize / 2}" cy="0" r="${
-      this.radius
-    }" transform="${this.rotate}" stroke-width="${this.strokeWidthpx}" />
-        <circle class="circle-progress" cx="${this.sqSize / 2}" cy="0" r="${
-      this.radius
-    }" stroke-width="${this.strokeWidthpx}" transform="${this.rotate}" style="${
-      this.strokeDashArray
-    }" />
+    return `<svg width="${this.sqSize}px" height="${
+      this.sqSize
+    }px" viewBox="${this.viewBox()}">
+        <circle class="circle-background" cx="${
+          this.sqSize / 2
+        }" cy="0" r="${this.radius()}" transform="${this.rotate()}" stroke-width="${this.strokeWidthpx()}" />
+        <circle class="circle-progress" cx="${
+          this.sqSize / 2
+        }" cy="0" r="${this.radius()}" stroke-width="${this.strokeWidthpx()}" transform="${this.rotate()}" style="${this.strokeDashArray()}" />
 
     </svg>
     <span class="circle-text">${this.percentage}%</span>`;
