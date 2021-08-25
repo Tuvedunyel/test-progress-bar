9b23d727b4d27dd9f9205f12142b31fe22abf1ec
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-08-25 09:22:36 +0200

    circle svg

--- novue.js
+++ novue.js
@@ -1,6 +1,14 @@
 const svgContainer = document.getElementById("progress-wrap");
 
-svgContainer.innerHTML = `<svg width="${sqSize}px" height="${sqSize}px" viewBox=${viewBox}></svg>`;
+svgContainer.innerHTML = `<svg width="${sqSize}px" height="${sqSize}px" viewBox=${viewBox}>
+    <circle class="circle-background" cx="${
+      sqSize / 2
+    }" cy="0" r="${radius}" transform="${rotate}" stroke-width="${strokeWidthpx}" />
+    <circle class="circle-progress" cx="${
+      sqSize / 2
+    }" cy="0" r="${radius}" stroke-width="${strokeWidthpx}" transform="${rotate}" style="${strokeDashArray}" />
+
+</svg>`;
 
 /* <svg
               v-bind:width="sqSize + 'px'"
