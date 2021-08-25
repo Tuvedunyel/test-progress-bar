71fc7d8ee3014a66a2cede87dcc31c6d7263f317
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-08-25 09:25:10 +0200

    const

--- novue.js
+++ novue.js
@@ -1,4 +1,7 @@
 const svgContainer = document.getElementById("progress-wrap");
+const sqSize = 200;
+let percentage = 0;
+const strokeWidth = 15;
 
 svgContainer.innerHTML = `<svg width="${sqSize}px" height="${sqSize}px" viewBox=${viewBox}>
     <circle class="circle-background" cx="${
@@ -9,27 +12,3 @@ svgContainer.innerHTML = `<svg width="${sqSize}px" height="${sqSize}px" viewBox=
     }" cy="0" r="${radius}" stroke-width="${strokeWidthpx}" transform="${rotate}" style="${strokeDashArray}" />
 
 </svg>`;
-
-/* <svg
-              v-bind:width="sqSize + 'px'"
-              v-bind:height="sqSize + 'px'"
-              v-bind:viewBox="viewBox"
-            >
-              <circle
-                class="circle-background"
-                v-bind:cx="sqSize / 2"
-                v-bind:cy="0"
-                v-bind:r="radius"
-                v-bind:transform="rotate"
-                v-bind:stroke-width="strokeWidthpx"
-              />
-              <circle
-                class="circle-progress"
-                v-bind:cx="sqSize / 2"
-                v-bind:cy="0"
-                v-bind:r="radius"
-                v-bind:stroke-width="strokeWidthpx"
-                v-bind:transform="rotate"
-                v-bind:style="strokeDashArray"
-              />
-            </svg> */
